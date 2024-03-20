8113fba97a51e4b90e9f237e517d1ec3f1a9b423
Sofia Protsiv <prochivsofia@gmail.com>
2024-03-20 17:47:37 +0200

    feat: add main logic

---
+++ controllers/auth/index.js
@@ -0,0 +1,18 @@
+import {controllerWrapper} from "../../decorators/index.js";
+import signIn from "./signIn.js";
+import signUp from "./signUp.js";
+import signOut from "./signOut.js";
+import getCurrent from "./getCurrent.js";
+import updateUserTheme from "./updateUserTheme.js";
+import updateProfile from "./updateProfile.js";
+import refresh from "./refresh.js";
+
+export default {
+    signUp: controllerWrapper(signUp),
+    signIn: controllerWrapper(signIn),
+    getCurrent: controllerWrapper(getCurrent),
+    signOut: controllerWrapper(signOut),
+    updateUserTheme: controllerWrapper(updateUserTheme),
+    updateProfile: controllerWrapper(updateProfile),
+    refresh: controllerWrapper(refresh)
+};
---
+++ routes/api/auth-router.js
@@ -0,0 +1,27 @@
+import express from 'express';
+
+import authController from "../../controllers/auth/index.js";
+
+import {authenticate, isEmptyBody, upload} from "../../middlewars/index.js";
+
+import {validateBody} from "../../decorators/index.js";
+
+import usersSchemas from "../../schemas/usersSchemas.js";
+
+const authRouter = express.Router();
+
+authRouter.post('/signup', validateBody(usersSchemas.userSignupSchema), authController.signUp);
+
+authRouter.post('/signin', validateBody(usersSchemas.userSigninSchema), authController.signIn);
+
+authRouter.post("/signout", authenticate, authController.signOut);
+
+authRouter.get("/current", authenticate, authController.getCurrent);
+
+authRouter.put("/update", authenticate, upload.single("avatar"), validateBody(usersSchemas.updateUserSchema), authController.updateProfile);
+
+authRouter.patch("/", authenticate, isEmptyBody, validateBody(usersSchemas.updateUserThemeSchema), authController.updateUserTheme);
+
+authRouter.post("/refresh", validateBody(usersSchemas.refreshSchema), authController.refresh);
+
+export default authRouter;
